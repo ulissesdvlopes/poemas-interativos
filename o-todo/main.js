5e2b65ccac3ef7af30d1c44159dcc24feb6e2908
Ulisses Lopes <ulissesdvlopes@gmail.com>
2022-06-11 04:49:58 -0300

    Adjusting touch event in "o todo"

--- o-todo/main.js
+++ o-todo/main.js
@@ -1,8 +1,8 @@
 let light = document.getElementById('light');
 
 const onMouseMove = (e) =>{
-    light.style.left = `calc(${e.pageX}px - 8vh)`;
-    light.style.top = `calc(${e.pageY}px - 8vh)`;
+    light.style.left = `calc(${e.pageX}px - 5vh)`;
+    light.style.top = `calc(${e.pageY}px - 12vh)`;
 }
   
   document.addEventListener('pointermove', onMouseMove);
