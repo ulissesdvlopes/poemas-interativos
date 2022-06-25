7fa04e75d5a92e3b6c41c10e95a14b615339a3b2
Ulisses Lopes <ulissesdvlopes@gmail.com>
2022-06-25 16:37:35 -0300

    Adding "carimbe" funcionality

--- carimbe/main.js
+++ carimbe/main.js
@@ -1,31 +1,72 @@
-function setup() {
-    createCanvas(window.innerWidth - 5, window.innerHeight - 5);
-    textSize(32);
-}
+let sketch = function(p) {
 
-function draw() {}
+  p.setup = function(){
+    p.createCanvas(window.innerWidth - 5, (window.innerHeight - 5)*0.6);
+  }
 
-function mousePressed() {
-  stamp();
-}
+  p.mousePressed = function(){
+    stamp(p);
+  }
 
-var angle = 45;
-var size = 32;
-var word = "hahaha";
+};
 
-function stamp() {
-    //stamp
-    push();
+new p5(sketch, 'canvas');
+
+let preview = function(p) {
+
+  p.setup = function(){
+    p.createCanvas(window.innerWidth - 5, (window.innerHeight - 5)*0.2);
+  }
+
+  p.draw = function(){
+    p.background(255);
+    stamp(p, p.width/2, p.height/2);
+  }
+
+};
+
+new p5(preview, 'preview');
+
+function stamp(p, posX, posY) {
+    if(!posX) {
+      posX = p.mouseX;
+      posY = p.mouseY;
+    } else {
+      
+    }
+    p.push();
     
-    angleMode(DEGREES);
-    translate(mouseX, mouseY);
+    p.angleMode(p.DEGREES);
+    p.translate(posX, posY);
     
-    textSize(size);
-    fill(255,0,0);
-    rotate(angle);
+    p.textSize(wordSize);
+    p.fill(wordColor);
+    p.rotate(angle);
     
-    textAlign(CENTER,CENTER);
-    text(word, 0, 0);
-    pop();
+    p.textAlign(p.CENTER,p.CENTER);
+    p.text(word, 0, 0);
+    p.pop();
     return false;
 }
+
+var angle = 0;
+var wordSize = 24;
+var word = "";
+var wordColor = "#000000";
+
+document.getElementById("content").addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  word = e.target.value;
+});
+document.getElementById("size").addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  wordSize = Number(e.target.value);
+});
+document.getElementById("rotation").addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  angle = Number(e.target.value);
+});
+document.getElementById("color").addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  wordColor = e.target.value;
+});
